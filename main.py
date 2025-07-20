b91b318626dfec7369d3e28c359b0f5678a570d3
Divetrainer <ian@divelab.tech>
2025-07-20 01:14:59 -0700

    updated to match the current progress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count, character_count
+from stats import word_count, character_count, dict_to_string
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -8,5 +8,13 @@ def get_book_text(file_path):
 file_path = "books/frankenstein.txt"
 document = get_book_text(file_path)
 num_words = word_count(document)
-print(f"{num_words} words found in the document")
-print(character_count(document))
+characters = character_count(document)
+sorted_chars = dict_to_string(characters)
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {file_path}...")
+print("----------- Word Count ----------")
+print(f"Found {num_words} total words")
+print("--------- Character Count -------")
+for char in sorted_chars:
+    print(f"{char["char"]}: {char["num"]}")
--- stats.py
+++ stats.py
@@ -13,8 +13,17 @@ def character_count(document):
                 char_report[char.lower()] +=1
             else:
                 char_report[char.lower()] = 1
-
     return char_report
 
-def sorted_list(character_list):
-    pass
+def sorted_list(char_list):
+    return char_list["num"]
+
+def dict_to_string(document):
+    updated_string = []
+    for letter in document:
+        counter = document[letter]
+        if letter.isalpha():
+            updated_dict = {"char": letter, "num": counter}
+            updated_string.append(updated_dict)
+    updated_string.sort(reverse=True, key=sorted_list)
+    return updated_string
